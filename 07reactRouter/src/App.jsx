dfc03a94f1705ebdd2e2685729b3e5ee0cef67d3
JD-011 <jayveerdahiya011@gmail.com>
2025-05-11 17:07:47 +0530

    React router crash course

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+
+function App() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
